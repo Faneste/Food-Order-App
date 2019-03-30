662a46872ad9d842cc1f2459845fbe119c40234a
Faneste <faneste12345@gmail.com>
2019-03-30 22:24:20 +0100

    dodati buttoni nezavrseni

--- App/food-order-app/src/components/foodBuilder/FoodBuilder.js
+++ App/food-order-app/src/components/foodBuilder/FoodBuilder.js
@@ -5,6 +5,7 @@ function FoodBuilder() {
   return (
     <div className="foodBuilder">
 
+      {/* title and text of the food building module */}
       <div className="foodBuilder__textContainer">
         <h2 className="foodBuilder__textContainer__title">Website main title</h2>
         <h3 className="foodBuilder__textContainer__text">
@@ -14,13 +15,56 @@ function FoodBuilder() {
         </h3>
       </div>
 
-        <div className="foodBuilder__ingredientsContainer">
-          <img className="foodBuilder__ingredientsContainer__pizzaDough" src={pizzaDough} alt="pizzaDough"/>
+      {/* image of pizza and other food */}
+      <div className="foodBuilder__ingredientsContainer">
+        <img className="foodBuilder__ingredientsContainer__pizzaDough" src={pizzaDough} alt="pizzaDough"/>
+      </div>
+
+      {/* buttons container */}
+      {/* menu of the type of food */}
+      <div className="foodBuilder__buttonsContainer">
+        <div className="foodBuilder__buttonsContainer__foodtypeMenu">
+          <button className="foodBuilder__buttonsContainer__foodtypeMenu__button">PIZZE</button>
+          <span className="foodBuilder__buttonsContainer__foodtypeMenu__span">~</span>
+          <button className="foodBuilder__buttonsContainer__foodtypeMenu__button">SENDVIČI</button>
+          <span className="foodBuilder__buttonsContainer__foodtypeMenu__span">~</span>
+          <button className="foodBuilder__buttonsContainer__foodtypeMenu__button">PITE</button>
+          <span className="foodBuilder__buttonsContainer__foodtypeMenu__span">~</span>
+          <button className="foodBuilder__buttonsContainer__foodtypeMenu__button">SALATE</button>
         </div>
 
-        <div className="foodBuilder__buttonsContainer">
-          
+        {/* type of pizza ingredients buttons */}
+        <div className="foodBuilder__ingredientsContainerbutton">
+          <div className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer">
+            <button className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__button">Sosevi</button>
+            <span className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__span">~</span>
+            <button className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__button">Sirevi</button>
+            <span className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__span">~</span>
+            <button className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__button">Meso</button>
+            <span className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__span">~</span>
+            <button className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__button">Povrće</button>
+            <span className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__span">~</span>
+            <button className="foodBuilder__ingredientsContainer__pizzaIngredientsMenuContainer__button">Začini</button>
+          </div>
         </div>
+
+        {/* ingredients for pizza buttons */}
+        <div className="foodBuilder__ingredientPizza">
+          <div className="foodBuilder__ingredientPizza__buttonContainer">
+            {/* pizza button image */}
+            <div className="foodBuilder__ingredientPizza__buttonContainer__imageContainer">
+              <img className="foodBuilder__ingredientPizza__buttonContainer__imageContainer__image"/>
+            </div>
+            {/* pizza button name */}
+            <div className="foodBuilder__ingredientPizza__buttonContainer__name">Pepperoni kobasica</div>
+            {/* pizza button description */}
+            <div className="foodBuilder__ingredientPizza__buttonContainer__description">Description</div>
+            {/* pizza button price */}
+            <div className="foodBuilder__ingredientPizza__buttonContainer__price">120 din</div>
+          </div>
+        </div>
+
+      </div>
 
     </div>
   )
--- App/food-order-app/src/components/footer/Footer.js
+++ App/food-order-app/src/components/footer/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
     <div className="footer">
       <div className="footer__container">
         <img className="footer__container__logo" src={footerLogo} alt="footerLogo"/>
-        <a href="#"><div className="footer__container__socialContainer" ><i className="fab fa-facebook-f"></i></div></a>
-        <a href="#"><div className="footer__container__socialContainer"><i className="fab fa-twitter"></i></div></a>
-        <a href="#"><div className="footer__container__socialContainer"><i className="fab fa-instagram"></i></div></a>
+        <a href="www.facebook.com"><div className="footer__container__socialContainer" ><i className="fab fa-facebook-f"></i></div></a>
+        <a href="www.twitter.com"><div className="footer__container__socialContainer"><i className="fab fa-twitter"></i></div></a>
+        <a href="www.instagram.com"><div className="footer__container__socialContainer"><i className="fab fa-instagram"></i></div></a>
         <img className="footer__container__basil" src={footerBasil} alt="footerBasil"/>
       </div>
     </div>
